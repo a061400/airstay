8653a341028eb282721a7090f8bccebdf0fe453c
shawnlin <a061400@gmail.com>
2024-08-09 10:39:39 +0800

    新增檢查用戶是否仍持續登入

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,6 +25,16 @@ export default {
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
     console.log(token);
     this.$http.defaults.headers.common.Authorization = token;
+    const api = `${process.env.VUE_APP_API}api/user/check`;
+    this.$http.post(api, this.user).then((res) => {
+      if (res.data.success) {
+        console.log('登入成功');
+        this.isLogin = true;
+      } else {
+        console.log('驗證錯誤(cookie失效)，請重新登入');
+        this.isLogin = false;
+      }
+    });
   },
   mounted() {
     emitter.on('login-status', (data) => {
