42f67c638cda66564291ba2393f8bb175afcdfc0
shawnlin <a061400@gmail.com>
2024-08-12 16:57:35 +0800

    畫面修改

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
+<!-- eslint-disable vuejs-accessibility/mouse-events-have-key-events -->
 <template>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <img alt="Vue logo" src="../assets/logo.png" style="width:25px">
@@ -9,24 +10,38 @@
           <router-link to="/" class="nav-link">變更地區語言</router-link>
           <router-link to="/" class="nav-link">變更幣別</router-link>
 
-          <!-- <router-link to="/login" v-if="!isLogin" class="nav-link">登入</router-link>
-          <a href="#" v-if="isLogin" @click.prevent="logout" class="nav-link">登出</a> -->
-
           <router-link to="/" class="nav-link">成為新會員</router-link>
-          <i v-if="!isLogin" class="bi bi-person" style="color:white; position: absolute;
-          top: 6px; right: 32px; font-size: 30px;"
-           @click.prevent="login" @keydown="handleKeyDown"></i>
-           <i v-if="isLogin" class="bi bi-person-check" style="color:white; position: absolute;
-          top: 6px; right: 32px; font-size: 30px;"
-          @click.prevent="logout" @keydown="handleKeyDown"></i>
-          <i v-if="isLogin" class="bi bi-house-heart" style="color:white; position: absolute;
-          top: 6px; right: 82px; font-size: 30px;"></i>
-          <i v-if="isLogin" class="bi bi-cart3" style="color:white; position: absolute;
-          top: 6px; right: 132px; font-size: 30px;"></i>
+
+          <!-- 登入圖示 -->
+          <i v-if="!isLogin"
+             class="bi bi-person"
+             :style="accountIconStyle"
+             @mouseover="hoverIcon(true, 'account')"
+             @mouseleave="hoverIcon(false, 'account')"
+             @click.prevent="login"
+             @keydown="handleKeyDown"></i>
+
+          <!-- 登出圖示 -->
+          <i v-if="isLogin" class="bi bi-person-check"
+             :style="accountIconStyle"
+             @mouseover="hoverIcon(true, 'account')"
+             @mouseleave="hoverIcon(false, 'account')"
+             @click.prevent="logout"
+             @keydown="handleKeyDown"></i>
+
+          <!-- 其他已登入圖示 -->
+          <i v-if="isLogin" class="bi bi-house-heart"
+          :style="likeIconStyle"
+             @mouseover="hoverIcon(true, 'like')"
+             @mouseleave="hoverIcon(false, 'like')"></i>
+          <i v-if="isLogin" class="bi bi-cart3"
+          :style="cartIconStyle"
+             @mouseover="hoverIcon(true, 'cart')"
+             @mouseleave="hoverIcon(false, 'cart')"></i>
         </div>
       </div>
     </div>
-</nav>
+  </nav>
 </template>
 
 <script>
@@ -42,9 +57,49 @@ export default {
   },
   data() {
     return {
+      isAccount: false,
+      isLike: false,
+      isCart: false,
     };
   },
+  computed: {
+    accountIconStyle() {
+      return {
+        color: this.isAccount ? 'gold' : 'white',
+        position: 'absolute',
+        top: this.isAccount ? '6px' : '6px',
+        right: this.isLogin ? '32px' : '32px', // 根據 isLogin 判斷右邊距離（可選）
+        fontSize: this.isAccount ? '30px' : '30px',
+        transition: 'all 0.3s ease',
+      };
+    },
+    likeIconStyle() {
+      return {
+        color: this.isLike ? 'gold' : 'white',
+        position: 'absolute',
+        top: this.isLike ? '6px' : '6px',
+        right: this.isLogin ? '82px' : '82px', // 根據 isLogin 判斷右邊距離（可選）
+        fontSize: this.isLike ? '30px' : '30px',
+        transition: 'all 0.3s ease',
+      };
+    },
+    cartIconStyle() {
+      return {
+        color: this.isCart ? 'gold' : 'white',
+        position: 'absolute',
+        top: this.isCart ? '6px' : '6px',
+        right: this.isLogin ? '132px' : '132px', // 根據 isLogin 判斷右邊距離（可選）
+        fontSize: this.isCart ? '30px' : '30px',
+        transition: 'all 0.3s ease',
+      };
+    },
+  },
   methods: {
+    hoverIcon(isHover, item) {
+      this.isAccount = isHover && (item === 'account');
+      this.isLike = isHover && (item === 'like');
+      this.isCart = isHover && (item === 'cart');
+    },
     logout() {
       const api = `${process.env.VUE_APP_API}logout`;
       this.$http.post(api)
