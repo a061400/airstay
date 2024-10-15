2964b8845f02563f66106977dd9db717f283fb4b
shawnlin <a061400@gmail.com>
2024-10-15 16:57:13 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:a061400/a061400.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:a061400/airstay.git master:gh-pages
 
 cd -
